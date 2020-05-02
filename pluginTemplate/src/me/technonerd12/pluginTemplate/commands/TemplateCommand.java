384e840853b30d7136088c00df7541b896b5c06a
Technonerd12 <gamemaker12123@gmail.com>
2020-05-01 19:30:47 -0600

    Updated Command Class

--- pluginTemplate/src/me/technonerd12/pluginTemplate/commands/TemplateCommand.java
+++ pluginTemplate/src/me/technonerd12/pluginTemplate/commands/TemplateCommand.java
@@ -3,6 +3,7 @@ package me.technonerd12.pluginTemplate.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import me.technonerd12.pluginTemplate.Main;
 
@@ -17,6 +18,10 @@ public class TemplateCommand implements CommandExecutor{
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (sender instanceof Player) {
+			Player p = (Player) sender;
+		}
+		
 		return false;
 	}
 	
